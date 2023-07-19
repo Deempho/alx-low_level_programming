1537d9f0aabea66396f866dbb1944131951f51dc
Deempho <ramotshidisidimpho@gmail.com>
2023-07-19 07:35:00 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_sign - checks for alphabetic character.
+ *@n:The character to be checked
+ *
+ * Return:1 if c is lowercase,0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('-');
+		return (1);
+	else if (n == 0)
+		_putchar('48);
+		return (0);
+	_putchar(' + ');
+	return (1);
+}
