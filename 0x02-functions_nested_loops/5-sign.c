55406fe7eda9e4418bc54e71932d1a336d88d87a
Deempho <ramotshidisidimpho@gmail.com>
2023-07-19 08:41:38 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,20 +7,21 @@
  * Return:1 if c is lowercase,0 otherwise
  */
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
-		_putchar('-');
-		return (1);
+	_putchar(43);
+	return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('48);
-		return (0);
+	_putchar('46);
+	return (0);
 	}
 	else
 	{
-	_putchar(' + ');
-	return (1);
+	_putchar(48);
+	return (0);
 	}
 }
