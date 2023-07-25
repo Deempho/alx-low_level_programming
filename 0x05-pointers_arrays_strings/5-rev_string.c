43d98e32f8cf88ea832f5f12bc7b5c9532f3a0df
Deempho <ramotshidisidimpho@gmail.com>
2023-07-25 10:07:03 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
  * rev_string - function that reverses a string.
- *@s: The string to be modified
+ *@s: print reverse string.
  *Return: void
  */
 
@@ -17,7 +17,7 @@ void rev_string(char *s)
 	for (index = len - 1; index >= len / 2; index--)
 	{
 		tmp = s[index];
-		s[index] s[len - index - 1];
+		s[index] = s[len - index - 1];
 		s[len - index - 1] = tmp;
 	}
 }
