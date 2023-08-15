8fd332d604459df75be04d52859b30a1393b41b7
Deempho <ramotshidisidimpho@gmail.com>
2023-08-15 13:42:09 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * FILE - 3-function_like_macro.h
+ *
+ * @x: input value( int)
+ *
+ * Desc: header file that compture absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
