a00df288ea9a43ab6706c622c9b01a5f00321e43
Deempho <ramotshidisidimpho@gmail.com>
2023-08-17 14:30:22 -0700

    new changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,13 @@
  *
  * Desc: first struct with alx
  */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
-
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
